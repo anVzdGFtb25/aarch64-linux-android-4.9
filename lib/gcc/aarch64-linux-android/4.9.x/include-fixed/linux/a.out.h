413d7c3ac92b6372b4b60c57111899301bcd73a9
Caroline Tice <cmtice@google.com>
2017-07-07 13:07:30 -0700

    Update prebuilt GCC to build 4162596.
    Bug: http://b/63044552
    
    Change-Id: Ieed34a1a13e3f44a62f206b5f01344c2698a42b7

--- lib/gcc/aarch64-linux-android/4.9.x/include-fixed/linux/a.out.h
+++ lib/gcc/aarch64-linux-android/4.9.x/include-fixed/linux/a.out.h
@@ -2,7 +2,7 @@
 
     It has been auto-edited by fixincludes from:
 
-	"/tmp/4496c72d3d0d6698c712d2ba9a7568cf/sysroot/usr/include/linux/a.out.h"
+	"/tmp/d1e993bc53e7176bed7b1e6ed5bb9072/sysroot/usr/include/linux/a.out.h"
 
     This had to be done to correct non-standard usages in the
     original, manufacturer supplied header file.  */
